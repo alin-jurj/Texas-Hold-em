862ad66043214be0dc5497a620b36a78744f0559
NextTime09 <alin_jurj@yahoo.com>
2021-05-08 16:25:33 +0300

    spectate

--- src/main/java/org/example/AfterLogin.java
+++ src/main/java/org/example/AfterLogin.java
@@ -5,12 +5,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.io.IOException;
 
 public class AfterLogin {
+    public int entryAmount1;
+    public int entryAmount2;
     private Image img;
     @FXML
     private Button logout;
@@ -20,7 +23,10 @@ public class AfterLogin {
     private ImageView playop;
     @FXML
     private ImageView spectateop;
-
+    @FXML
+    private ChoiceBox<Integer> entryPlay = new ChoiceBox<>();
+    @FXML
+    private ChoiceBox<Integer> entrySpectate = new ChoiceBox<>();
     public void initialize(){
         img = new Image(getClass().getResourceAsStream("/img/Loginimage.png"));
         bimg.setImage(img);
@@ -28,13 +34,48 @@ public class AfterLogin {
         img = new Image(getClass().getResourceAsStream("/img/chips-cropped.png"));
         playop.setImage(img);
 
-        img = new Image(getClass().getResourceAsStream("/img/chips-cropped.png"));
+        img = new Image(getClass().getResourceAsStream("/img/spectate.png"));
         spectateop.setImage(img);
 
+        entrySpectate.setValue(500);
+        entryPlay.setValue(500);
+        addValues(entryPlay);
+        addValues(entrySpectate);
 
+
+    }
+    public void addValues(ChoiceBox<Integer> entry) {
+        entry.getItems().add(500);
+        entry.getItems().add(1000);
+        entry.getItems().add(2500);
+        entry.getItems().add(5000);
+        entry.getItems().add(10000);
+        entry.getItems().add(25000);
+        entry.getItems().add(50000);
+        entry.getItems().add(100000);
+        entry.getItems().add(500000);
+        entry.getItems().add(1000000);
     }
     public void userLogOut(ActionEvent event) throws IOException{
         App m=new App();
         m.changeScene("LogIn.fxml");
     }
+    public void getChoice(ChoiceBox<Integer> entry){
+        entryAmount1 = entry.getValue();
+        entryAmount2=entry.getValue();
+    }
+    public void Playbutton() throws IOException
+    {
+        App m = new App();
+        getChoice(entryPlay);
+        m.changeScene("match.fxml");
+    }
+    public int getEntryPlay(){ return entryAmount1; }
+    public void Spectatebutton() throws IOException
+    {
+        App m = new App();
+        getChoice(entrySpectate);
+        //m.changeScene("match.fxml");
+    }
+    public int getEntrySpectate(){ return entryAmount2; }
 }
