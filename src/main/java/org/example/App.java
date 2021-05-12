ce5cca7e44f800ef980da2e6c0301959a602d6a9
zkato <Zkatona51@yahoo.com>
2021-05-12 18:25:51 +0300

    TH-7 Done (Matchul in sine)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -19,7 +19,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         stg=primaryStage;
-        scene = new Scene(loadFXML("entry"));
+        scene = new Scene(loadFXML("Admin"));
         primaryStage.setResizable(false);
         primaryStage.setTitle("Texas Hold'em");
         primaryStage.setScene(scene);
@@ -28,6 +28,11 @@ public class App extends Application {
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
+    }
+
+    public void resizeStage(int width, int height){
+        stg.setWidth(width);
+        stg.setHeight(height);
     }
 
 
